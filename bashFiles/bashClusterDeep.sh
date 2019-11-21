7a7e8cdec27741d711f6c326ebd9e9dcdb8a470d
Unknown <dogasiyli@gmail.com>
2019-11-21 16:08:47 +0300

    bash better

--- bashFiles/bashClusterDeep.sh
+++ bashFiles/bashClusterDeep.sh
@@ -47,6 +47,8 @@ then
     echo here222
     python ./clusterDeep.py --trainMode cosae --posterior_dim 32 --dataToUse skeleton --applyCorr 2 --pcaCount 64 --numOfSigns 11 --epochs 100 --appendEpochBinary 1 --randomSeed 5
   }
+elif [ $curHost == "doga-MSISSD" ];
+then
 else
   declare -i i=0
   declare -a posterior_dim_array
@@ -57,7 +59,55 @@ else
   numOfSigns_array=(11 41)
   randomSeed=5
   appendEpochBinary=0
-  posterior_dim_array=(32 128) # 64 256
+  posterior_dim_array=(256 512) # 32 64 128
+  rnnDataMode_array=(lookBack) # patchPerVideo frameOverlap
+  epochs=$epochCountToRun
+  for numOfSigns in ${numOfSigns_array[@]}
+  do
+    for trainMode in cosae corsa
+    do
+      for dataToUse in skeleton hog sn
+      do
+        [ "$dataToUse" == "skeleton" ] &&
+        {
+          pcaCount_array=(32 64 96) #64 96
+        } ||
+        {
+          pcaCount_array=(256 512) #512 1024
+        }
+        for pcaCount in ${pcaCount_array[@]}
+        do
+          for posterior_dim in ${posterior_dim_array[@]}
+          do
+            [ "$trainMode" == "cosae" ] &&
+            {
+              echo $i $trainMode $dataToUse $pcaCount $posterior_dim $applyCorr $numOfSigns
+              python ./clusterDeep.py --trainMode $trainMode --posterior_dim $posterior_dim --dataToUse $dataToUse --applyCorr $applyCorr --pcaCount $pcaCount --numOfSigns $numOfSigns --epochs $epochs --appendEpochBinary $appendEpochBinary --randomSeed $randomSeed
+            } ||
+            {
+              for rnnDataMode in ${rnnDataMode_array[@]}
+              do
+                echo $i $trainMode $dataToUse $pcaCount $posterior_dim $applyCorr $rnnDataMode $numOfSigns
+                python ./clusterDeep.py --trainMode $trainMode --rnnDataMode $rnnDataMode --posterior_dim $posterior_dim --dataToUse $dataToUse --applyCorr $applyCorr --pcaCount $pcaCount --numOfSigns $numOfSigns --epochs $epochs --appendEpochBinary $appendEpochBinary --randomSeed $randomSeed
+              done
+            }
+            i=$((i+1))
+          done
+        done
+      done
+    done
+  done
+else
+  declare -i i=0
+  declare -a posterior_dim_array
+  declare -a pcaCount_array
+  declare -a rnnDataMode_array
+  declare -a numOfSigns_array
+  applyCorr=2
+  numOfSigns_array=(11 41)
+  randomSeed=5
+  appendEpochBinary=0
+  posterior_dim_array=(256 512) # 64 256
   rnnDataMode_array=(lookBack) # patchPerVideo frameOverlap
   epochs=$epochCountToRun
   for numOfSigns in ${numOfSigns_array[@]}
