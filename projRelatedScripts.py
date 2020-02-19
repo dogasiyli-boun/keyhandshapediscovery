8cd2dcb908b2e095ea8870f7ae747b1782cb4316
Recep Doğa SİYLİ <dogasiyli@gmail.com>
2020-02-20 00:47:22 +0300

    minor fix
    fix on clustCntVec and dimsArray confusion in my head

--- projRelatedScripts.py
+++ projRelatedScripts.py
@@ -70,21 +70,22 @@ def run4All_createData(dataToUseArr=["hog", "skeleton", "sn"], sign_countArr=[11
             checkCreateData2Use(sign_count=sign_count, dataToUse=dataToUse, recreate=False, recreate_hog=False)
 
 def runForBaseClusterResults(normMode, randomSeed=5, clusterModels=['KMeans', 'GMM_diag'],
-                             dataToUseArr=["hog", "skeleton", "sn"], numOfSignsArr=[11], dimArray=None):
+                             dataToUseArr=["hog", "skeleton", "sn"], numOfSignsArr=[11], clustCntVec=None):
     data_dir = funcH.getVariableByComputerName('data_dir')
     results_dir = funcH.getVariableByComputerName('results_dir')
 
     for dataToUse in dataToUseArr:
         for numOfSigns in numOfSignsArr:
-            if dimArray is None:
-                if dataToUse == 'skeleton':
-                    dimArray = [32, 64, 96]
-                else:  # dataToUse == 'sn' or dataToUse == 'hog':
-                    dimArray = [256]
+            if clustCntVec is None:
+                clustCntVec = [64, 128, 256]
+            if dataToUse == 'skeleton':
+                dimArray = [32, 64, 96]
+            else:  # dataToUse == 'sn' or dataToUse == 'hog':
+                dimArray = [256]
             for dims in dimArray:
                 funcHP.runClusteringOnFeatSet(data_dir=data_dir, results_dir=results_dir, dataToUse=dataToUse,
                                               normMode=normMode, numOfSigns=numOfSigns, pcaCount=dims,
-                                              expectedFileType='Data', clustCntVec=dimArray, clusterModels=clusterModels,
+                                              expectedFileType='Data', clustCntVec=clustCntVec, clusterModels=clusterModels,
                                               randomSeed=randomSeed)
 
 #
